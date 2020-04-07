c150593ce22f70ea1edee86066c1625f052480b6
unknown <nahuugh98@gmail.com>
2020-04-07 00:04:25 -0300

    V1.1.0

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int suma(int,int);
+int resta(int,int);
+int multiplicacion(int,int);
+float division(int,int);
+
 int main()
 {
     int num1;
     int num2;
-    int suma;
-    int resta;
+    int sum;
+    int res;
     int mult;
-    int div;
+    float div;
 
     printf("CALCULADORA\n\n");
     printf("Ingrese primer numero: ");
@@ -16,16 +21,52 @@ int main()
     printf("Ingrese segundo numero: ");
     scanf("%d",&num2);
 
-    suma=num1+num2;
-    resta=num1-num2;
-    mult=num1*num2;
-    div=num1/num2;
+    sum=suma(num1,num2);
+    res=resta(num1,num2);
+    mult=multiplicacion(num1,num2);
+    div=division(num1,num2);
 
-    printf("\nLa suma es: %d",suma);
-    printf("\nLa resta es: %d",resta);
+    printf("\nLa suma es: %d",sum);
+    printf("\nLa resta es: %d",res);
     printf("\nLa multiplicacion es: %d",mult);
-    printf("\nLa division es: %d",div);
+    printf("\nLa division es: %.2f",div);
 
 
     return 0;
+}
+
+int suma(int numero1, int numero2)
+{
+    int resultado;
+
+    resultado=numero1+numero2;
+
+    return resultado;
+}
+
+int resta(int numero1, int numero2)
+{
+    int resultado;
+
+    resultado=numero1-numero2;
+
+    return resultado;
+}
+
+int multiplicacion(int numero1, int numero2)
+{
+    int resultado;
+
+    resultado=numero1*numero2;
+
+    return resultado;
+}
+
+float division(int numero1, int numero2)
+{
+    float resultado;
+
+    resultado=numero1/numero2;
+
+    return resultado;
 }
