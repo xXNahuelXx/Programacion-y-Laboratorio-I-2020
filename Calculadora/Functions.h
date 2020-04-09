b6a33cec59d43741f5c575b8e68c8441d31fa9c0
unknown <nahuugh98@gmail.com>
2020-04-08 23:14:43 -0300

    1.3.1

---
+++ Calculadora/Functions.h
@@ -0,0 +1,10 @@
+
+
+float suma(float,float);
+float resta(float,float);
+float multiplicacion(float,float);
+float division(float,float);
+unsigned long long int factorial(int);
+float pedirNumero(char[]);
+void informes(float,float,float,float,float,float,int,int,char[]);
+void menu();
